ba646799c57f566ed90e0e226afebf1e6e3d0995
ucchy <ocean163@hotmail.com>
2015-01-28 09:46:08 +0900

    UndineMailer v1.0.0 : Updated build script.

--- build.cmd
+++ build.cmd
@@ -1,30 +1,37 @@
 @echo off
 pushd %~dp0
 
-echo ===== clean up =====
+echo ----- clean up -----
 if exist release rmdir /s /q release
 mkdir release
 
-echo ===== making release package for BukkitDev =====
+echo ----- making release package for BukkitDev -----
 move /y pom.xml pom.xml.backup
 java -jar XmlSetter.jar pom.xml.backup pom.xml release.lang en
 call mvn clean deploy
+if errorlevel 1 goto errorend
 pushd target
 ren UndineMailer-*-dist.zip UndineMailer-*-en.zip
 popd
 move /y target\UndineMailer-*-en.zip release\
 
-echo ===== making release package for Japan User Forum =====
+echo ----- making release package for Japan User Forum -----
 java -jar XmlSetter.jar pom.xml.backup pom.xml release.lang ja
 call mvn clean javadoc:jar source:jar deploy
+if errorlevel 1 goto errorend
 pushd target
 ren UndineMailer-*-dist.zip UndineMailer-*-ja.zip
 popd
 move /y target\UndineMailer-*-ja.zip release\
 
-echo ===== finalize =====
+echo ----- finalize -----
 move /y pom.xml.backup pom.xml
 
-echo ===== end =====
+echo ===== Succeeded! =====
+goto end
 
+:errorend
+echo ===== Failed! Please check the build error logs =====
+
+:end
 popd
